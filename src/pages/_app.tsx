7f94b168a49cf6c2b8e65345730223c7b84fe2f8
tarupathak <pathaktaru2002@gmail.com>
2025-01-06 17:38:55 +0530

    Added : All files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,3 +23,5 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
+
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,23 +1,48 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
-} from 'wagmi/chains';
+import { Chain, getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { mainnet } from "wagmi/chains";
+import "@rainbow-me/rainbowkit/styles.css";
+
+const botanixChain: Chain = {
+  id: 3636,
+  name: "Botanix Testnet",
+  // network: "BTC",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Botanix Testnet",
+    symbol: "BTC",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://node.botanixlabs.dev"],
+    },
+    public: {
+      http: ["https://node.botanixlabs.dev"],
+    },
+  },
+};
+
+// export const sepoliaChain: Chain = {
+//   id: 11155111,
+//   name: "Sepolia Testnet",
+//   // iconUrl: "/img/b2.png",
+//   nativeCurrency: {
+//     decimals: 18,
+//     name: "Sepolia Testnet",
+//     symbol: "ETH",
+//   },
+//   rpcUrls: {
+//     default: {
+//       http: ["https://sepolia.infura.io/v3/ad9cef41c9c844a7b54d10be24d416e5"],
+//     },
+//     public: {
+//       http: ["https://sepolia.infura.io/v3/ad9cef41c9c844a7b54d10be24d416e5"],
+//     },
+//   },
+// };
 
 export const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
-  ],
-  ssr: true,
+  appName: "RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [botanixChain],
+  // ssr: true,
 });
